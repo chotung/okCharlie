61cb0cff41b47df240dd80ad39c0842c7b3d3c8d
Charlie Tung <choelestung@gmail.com>
2020-02-17 17:10:49 -0500

    setting up route for likes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express")
 const app = express()
 require("dotenv").config();
-const db = require("./models")
-const { Charlie, User } = db
-
 const {HOST, PORT } = process.env;
+const mongoose = require("mongoose")
+const User = require("./models/User")
 
 app.use(
   express.urlencoded({
@@ -13,94 +12,42 @@ app.use(
 );
 app.use(express.json());
 
+// DATABASE CONFIGS
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "Connection Error:"))
+db.once("open", () => {
+  console.log("We're connected!")
+})
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 
-app.use("/api", require("./routes/users.js"));
-app.use("/api", require("./routes/charlies.js"));
+// app.use("/api", require("./routes/users.js"));
+// app.use("/api", require("./routes/charlies.js"));
 
-app.get("/test", async (req, res) => {
-  const user = await User.findByPk(1, {
-    includes: ['charlie']
+app.get("/seed", (req, res) => {
+  const seed = [
+    {name: "Vanilla Charlie"},
+    {name: "Chocolate Charlie"},
+    {name: "Strawberry Charlie"},
+  ]
+
+  User.create(seed, (err, listOfCharlies)=> {
+    if(err) throw err
+
+    console.log(listOfCharlies[0])
+    console.log(listOfCharlies[1])
+    console.log(listOfCharlies[2])
   })
-  console.log(user.get())
-})
-app.get("/test2", async (req, res) => {
-  const charlie = await Charlie.findByPk(1, {
-    includes: ['user']
-  })
-  console.log(charlie.get())
 })
 
 
-app.get("*", async (req, res) => {
-  res.send("Welcome to the Homepage")
-})
-
-
-const seed = () => {
-  return Promise.all([
-    Charlie.create({name: "Vanilla Charlie"}),
-    User.create({
-      name: "Laura",
-      age: 18,
-      email: "hotsingle@gmail.com",
-      sex: "female"
-    }),
-    User.create({
-      name: "Lara",
-      age: 18,
-      email: "hotsingle@gma.com",
-      sex: "female"
-    }),
-    User.create({
-      name: "Laur",
-      age: 18,
-      email: "hotsingle@gmai.com",
-      sex: "female"
-    }),
-    User.create({
-      name: "Aura",
-      age: 18,
-      email: "hotsingle@gmil.com",
-      sex: "female"
-    }),
-    User.create({
-      name: "Lena",
-      age: 18,
-      email: "hotsinge@gmail.com",
-      sex: "female"
-    }),
-    User.create({
-      name: "Len",
-      age: 18,
-      email: "hotsigle@gmail.com",
-      sex: "female"
-    }),
-    User.create({
-      name: "Ena",
-      age: 18,
-      email: "hosingle@gmail.com",
-      sex: "female"
-    })
-  ])
-  .catch(err => console.log(err))
-}
-
-
-///////////////////////
-db.sequelize.sync({ 
-  force: true,
-  logging: false 
-})
-.then(() => {
-  seed();
-})
-
-.then(() => {
-  app.listen(PORT, function() {
-    console.log(`Server is listening on http://${HOST}:${PORT}`);
-  })
+app.listen(PORT, function() {
+  console.log(`Server is listening on http://${HOST}:${PORT}`);
 })
