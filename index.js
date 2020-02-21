7003836d5278adebe535114d7c8f0291718ac1ea
Charlie Tung <choelestung@gmail.com>
2020-02-21 10:26:59 -0500

    initial deploy

--- index.js
+++ index.js
@@ -174,6 +174,9 @@ app.delete("/deleteall", async (req, res) => {
 })
 
 
+app.get("*", (req, res) => {
+  res.send("Hello World") 
+})
 app.listen(PORT, function() {
   console.log(`Server is listening on http://${HOST}:${PORT}`);
 })
